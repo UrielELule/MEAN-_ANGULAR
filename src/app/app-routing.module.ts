6a13bb142fc1e1a35b78d97cb40c9cd474d9043b
UrielELule <archi4103@gmail.com>
2021-04-11 22:31:50 -0500

    rutas padre e hijas -Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,50 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
 
-import { PagesComponent } from './pages/pages.component';
 
 const routes: Routes = [
   /*RUTAS HIJAS EN PAGE COMPONENT RUTAS PROTEGIDAS*/ 
+  
+  //path : './dashboard pagesRouting';
+  //path: '/auth authRouting';
   {
     path: '',
-    component: PagesComponent,
-    children: [
-
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: 'progress',
-        component: ProgressComponent
-      },
-      {
-        path: 'grafica1',
-        component: Grafica1Component
-      },
-      {
-        path:'',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      }
-
-    ]
-  },
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'register',
-    component: RegisterComponent
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
   },
   {
     path: '**',
@@ -58,7 +29,9 @@ const routes: Routes = [
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports:[RouterModule]
 })
